f7c4bcf1739c0323b6383abc4306487a4b6b2099
Darryl <darrylmangibin@gmail.com>
2019-01-14 23:05:09 +0800

    Added a menu

--- functions.php
+++ functions.php
@@ -5,9 +5,18 @@ function lapizzeria_styles() {
 	wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0');
 	wp_enqueue_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0');
 
+
 }
 
 add_action('wp_enqueue_scripts', 'lapizzeria_styles');
+
+// add menus
+function lapizzeria_menus() {
+	register_nav_menus(array(
+		'header-menu' => 'Header Menu',
+	));
+}
+add_action('init', 'lapizzeria_menus');
 
 
 
