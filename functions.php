0b778249f798cb3c1776b199cb5c1972a37d23f2
Darryl <darrylmangibin@gmail.com>
2019-01-17 19:19:18 +0800

    added font-awesome

--- functions.php
+++ functions.php
@@ -1,39 +1,21 @@
 <?php 
 
 function lapizzeria_styles() {
-	//adding stylesheets
+	// adding stylesheet
 	wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0');
 	wp_enqueue_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0');
-
-
+	
 }
-
 add_action('wp_enqueue_scripts', 'lapizzeria_styles');
 
 // add menus
-function lapizzeria_menus() {
-	register_nav_menus(array(
-		'header-menu' => 'Header Menu',
-		'social-menu' => 'Social Menu'
-	));
+function lapizzeria_menus(){
+	register_nav_menus( array( 
+		'header-menu' => __('Header Menu'),
+		'social-menu' => __('Social Menu')
+	 ) );
 }
 add_action('init', 'lapizzeria_menus');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- header.php
+++ header.php
@@ -1,54 +1,55 @@
 <!doctype html>
 <html class="no-js" lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?php wp_head(); ?>
-    </head>
-    <body>
+  <head>
+      <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title></title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <?php wp_head() ?>
+  </head>
+  <body>
 
-    	<header class="site-header">
-    		<div class="container">
-	    		<div class="logo">
-	    			<a href="<?php echo esc_url(  home_url()); ?>">
-	    				<img class="logoimage" src="<?php echo get_template_directory_uri() ?>/img/logo.svg">
-	    			</a>
-	    		</div> <!-- logo -->
-	    		<div class="header-information">
-	    			<div class="socials">
-	    				<?php 
-	    					$args = array(
-	    						'theme_location' => 'social-menu',
-	    						'container' => 'nav',
-	    						'container_class' => 'socials',
-	    						'container_id' => 'socials',
-	    						'link_before' => '<span class="sr-text"',
-	    						'link_after' => '</span>'
-	    					);
-	    					wp_nav_menu($args);
-	    				 ?>
-	    			</div><!--socials -->
-	    			<div class="addres">
-	    				<p>8491 Bay Avenue View, CA 9043</p>
-	    				<p>Phome NUmber +6306-4856-933</p>
-	    			</div> <!-- header-information -->
-	    		</div>
-    		</div>
-    	</header>
+  <header class="site-header">
+    <div class="container">
+      <div class="logo">
+        <a href="<?php echo esc_url( home_url('/') ); ?>">
+          <img class="logoimage" src="<?php echo get_template_directory_uri() ?>/img/logo.svg">
+        </a>
+      </div> <!-- logo -->
+      <div class="header-information">
+        <div class="socials">
+          <?php 
+            $args = array(
+              'theme_location' => 'social-menu',
+              'container' => 'nav',
+              'container_class' => 'socials',
+              'container_id' => 'socials',
+              'link_before' => '<span class="sr-text">',
+              'link_after' => '</span>'
+            );
+            wp_nav_menu($args);
+          ?>
+        </div><!-- socials -->
+        <div class="address">
+          <p>8179 Bay Avenue Mountain View, Ca 948443</p>
+          <p>Phone Number: +1-92-456-7890</p>
+        </div>
+      </div><!-- header-information -->
+    </div><!-- container -->
+  </header>
 
-    	<div class="main-menu">
-    		<div class="navigation">
-    			<!-- add menu to the UI -->
-    			<?php 
-    				$args = array(
-    					'theme_location' => 'header-menu',
-    					'container' => 'nav',
-    					'container_class' => 'site-nav',
-    				);
-    				wp_nav_menu($args);
-    			 ?>
-    		</div>
-    	</div>
+  <div class="main-menu">
+    <div class="navigation">
+      <?php 
+        $args = array(
+          'theme_location' => 'header-menu',
+          'container' => 'nav',
+          'container_class' => 'site-nav'
+        );
+        wp_nav_menu($args);
+       ?>
+    </div>
+  </div>
+
+    	
