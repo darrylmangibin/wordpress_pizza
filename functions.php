d1cd2b93bf9ccc2a917fccd518f9af6ecc4bbdeb
Darryl <darrylmangibin@gmail.com>
2019-01-18 14:07:36 +0800

    Added a custom post type in functions.php

--- functions.php
+++ functions.php
@@ -36,7 +36,64 @@ add_action('init', 'lapizzeria_menus');
 
 
 
-
+function custom_post_type() {
+ 
+// Set UI labels for Custom Post Type
+    $labels = array(
+        'name'                => _x( 'Pizzas', 'Lapizzeria' ),
+        'singular_name'       => _x( 'Pizzas', 'Lapizzeria' ),
+        'menu_name'           => __( 'Lapizzeria', 'lapizzaeria' ),
+        'parent_item_colon'   => __( 'Parent menu', 'lapizzeria' ),
+        'all_items'           => __( 'All Menus', 'lapizzeria' ),
+        'view_item'           => __( 'View Menu', 'lapizzeria' ),
+        'add_new_item'        => __( 'Add New menu', 'lapizzeria' ),
+        'add_new'             => __( 'Add New', 'lapizzeria' ),
+        'edit_item'           => __( 'Edit menu', 'lapizzeria' ),
+        'update_item'         => __( 'Update menu', 'lapizzeria' ),
+        'search_items'        => __( 'Search menu', 'lapizzeria' ),
+        'not_found'           => __( 'Not Found', 'lapizzeria' ),
+        'not_found_in_trash'  => __( 'Not found in Trash', 'lapizzeria' ),
+    );
+     
+// Set other options for Custom Post Type
+     
+    $args = array(
+        'label'               => __( 'menus', 'lapizzeria' ),
+        'description'         => __( 'description', 'lapizzeria' ),
+        'labels'              => $labels,
+        // Features this CPT supports in Post Editor
+        'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+        // You can associate this CPT with a taxonomy or custom taxonomy. 
+        'taxonomies'          => array( 'genres' ),
+        /* A hierarchical CPT is like Pages and can have
+        * Parent and child items. A non-hierarchical CPT
+        * is like Posts.
+        */ 
+        'hierarchical'        => false,
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'show_in_nav_menus'   => true,
+        'show_in_admin_bar'   => true,
+        'menu_position'       => 5,
+        'can_export'          => true,
+        'has_archive'         => true,
+        'exclude_from_search' => false,
+        'publicly_queryable'  => true,
+        'capability_type'     => 'page',
+    );
+     
+    // Registering your Custom Post Type
+    register_post_type( 'specialties', $args );
+ 
+}
+ 
+/* Hook into the 'init' action so that the function
+* Containing our post type registration is not 
+* unnecessarily executed. 
+*/
+ 
+add_action( 'init', 'custom_post_type', 0 );
 
 
  ?>
